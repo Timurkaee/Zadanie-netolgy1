87af1d83ec329b0338a6cd0806b75ce4cc2a062d
Timur Sharippaev <shtil777@gmail.com>
2023-02-23 12:04:42 +0300

    Change type int to long

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[]{10, 2, 400, 10});
-        int max = salesManager.max();
+        SalesManager salesManager = new SalesManager(new long[]{10, 2, 400, 10});
+        long max = salesManager.max();
         System.out.println(max);
 
     }
