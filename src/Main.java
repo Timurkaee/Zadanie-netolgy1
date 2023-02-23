ac980576fa2d03e596113bc33321f956ee99d6cb
Timur Sharippaev <shtil777@gmail.com>
2023-02-23 12:42:57 +0300

    adding min func& average func

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         SalesManager salesManager = new SalesManager(new int[]{10, 2, 400, 10});
+
         int max = salesManager.max();
         System.out.println(max);
 
+        int min = salesManager.min();
+        System.out.println(min);
+        int averageOfSales = salesManager.averageOfSales();
+        System.out.println(averageOfSales);
     }
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,21 @@ public class SalesManager {
         }
         return max;
     }
+    public int min() {
+        int min = Integer.MAX_VALUE;
+        for (int sale : sales) {
+            if (min > sale) {
+                min = sale;
+            }
+        }
+        return min;
+    }
+
+    public int averageOfSales() {
+     int summa= 0;
+     for (int sale:sales){
+       summa += sale;
+     }
+     return (summa-max()-min())/(sales.length-2);
+    }
 }
